d7d41a5ca3f064d6783dcf36ec7f0982872bbc6f
SANDARU-IT\User <maggonageabenayake@gmail.com>
2025-03-10 17:57:17 +0530

    Fourth Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import CounterClass from './components/Counterclass';
+import CounterFunction from './components/CounterFunction';
 
 function App() {
   return (
     <div className="App">
       <h1> Hello React</h1>
+      <CounterClass/>
+      <CounterFunction/>
     </div>
   );
 }
