0159da76541b29d6fee0e8c65b73f36b287e26f7
SANDARU-IT\User <maggonageabenayake@gmail.com>
2025-03-10 15:55:54 +0530

    Thired Commit

---
+++ frontend/src/components/Counterclass.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class CounterClass extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            number :0
+        }
+    }
+    render(){
+        return (
+            <div>
+               <h1>Counter = {this.state.number}</h1>
+            </div>
+        )
+    }
+}
+
+export default CounterClass; 
