1c84900e46646e4b74df14bf0e29991cc9404b9d
SANDARU-IT\User <maggonageabenayake@gmail.com>
2025-03-23 21:22:28 +0530

    sixth 1.1 commit

--- frontend/src/components/AddStudent.js
+++ frontend/src/components/AddStudent.js
@@ -73,14 +73,14 @@ export default function AddStudent() {
 
         <div class="form-group">
           <label for="gender">Student Gender</label>
-          <input
-            type="text"
-            class="form-control"
-            id="gender"
-            value={gender}
-            placeholder="Enter Student Gender"
-            onChange={(e)=>setGender(e.target.value)}
-          />
+          <select
+                            className="form-control"
+                            value={gender}
+                            onChange={(e) =>setGender(e.target.value)}
+                        >
+                            <option value="Male">Male</option>
+                            <option value="Female">Female</option>
+                        </select>
         </div>
 
         <button type="submit" class="btn btn-primary" onClick={submitStudent}>
